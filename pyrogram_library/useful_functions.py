e674384543e57c5d080233926f05bcb633f38ecc
KateBelova <ka7e.online@gmail.com>
2025-06-04 20:29:36 +0300

    tiny change

--- pyrogram_library/useful_functions.py
+++ pyrogram_library/useful_functions.py
@@ -4,8 +4,8 @@ import os
 def get_folder_size_and_count(filepath):
     count = sum(len(files) for root, dirs, files in os.walk(filepath))
     total_size = sum(
-        os.path.getsize(os.path.join(root, f))
+        os.path.getsize(os.path.join(root, file))
         for root, dirs, files in os.walk(filepath)
-        for f in files
+        for file in files
     )
     return count, total_size
